37ab21130c218472676fc1dd999b4a173cec0902
AliceDelpuppo <alicedelpuppo1@gmail.com>
2024-04-06 17:11:39 -0300

    task 3

--- sections/Content/Cupom.tsx
+++ sections/Content/Cupom.tsx
@@ -7,7 +7,7 @@ export default function Cupom(
   { cupomCode, cupomDescription }: Props,
 ) {
   return (
-    <div class="flex flex-col gap-2 pl-3 container my-10 ">
+    <div class="flex flex-col gap-2 pl-3 container my-12 ">
       <p class="text-sm font-bold w-[150px] text-center p-2 border-dashed border border-black hover:bg-black hover:text-white hover:boder-white duration-300">
         {cupomCode}
       </p>
